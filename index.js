34478362a136c63edd623e5ea62326aeb3e23519
Vishwas Shashidhar <ivishwas.s@gmail.com>
2017-09-25 21:08:51 +0530

    added support for best case match for country and region

--- index.js
+++ index.js
@@ -1,31 +1,77 @@
 'use strict';
 
 const _ = require('underscore');
+const stringSimilarity = require('string-similarity');
 
 const countries = require('./data/countries.json');
 const regions = require('./data/regions.json');
 
+const countryNames = _.pluck(countries, 'name');
+const regionNames = _.pluck(regions, 'name');
+
 exports.find = {
 
     country: (searchString) => {
+        
         if (!searchString) {
             return undefined;
         }
 
-        return _.find(countries, (item) => {
-            return item.cca2.toLowerCase() === searchString.toLowerCase() || item.name.toLowerCase() === searchString.toLowerCase();
+        var sString = searchString;
+
+        var result = _.find(countries, (item) => {
+            return item.cca2.toLowerCase() === sString.toLowerCase() || item.name.toLowerCase() === sString.toLowerCase();
         });
+
+        if (!result) {
+
+            var similarity = stringSimilarity.findBestMatch(searchString, countryNames);            
+
+            if (similarity.bestMatch.rating < 0.5) {
+                return;
+            }
+
+            sString = similarity.bestMatch.target;
+
+            result = _.find(countries, (item) => {
+                return item.cca2.toLowerCase() === sString.toLowerCase() || item.name.toLowerCase() === sString.toLowerCase();
+            });
+
+        }
+
+        return result;
 
     },
 
     region: (searchString) => {
+
         if (!searchString) {
             return undefined;
         }
 
-        return _.find(regions, (item) => {
-            return item.cca2.toLowerCase() === searchString.toLowerCase() || item.name.toLowerCase() === searchString.toLowerCase();
+        var sString = searchString;
+        
+        var result = _.find(regions, (item) => {
+            return item.cca2.toLowerCase() === sString.toLowerCase() || item.name.toLowerCase() === sString.toLowerCase();
         });
+
+        if (!result) {
+
+            var similarity = stringSimilarity.findBestMatch(searchString, regionNames);
+            
+            if (similarity.bestMatch.rating < 0.5) {
+                return;
+            }
+
+            sString = similarity.bestMatch.target;
+
+            result = _.find(regions, (item) => {
+                return item.cca2.toLowerCase() === sString.toLowerCase() || item.name.toLowerCase() === sString.toLowerCase();
+            });
+        }
+
+        return result;
+
     }
 
 };
--- test/index.test.js
+++ test/index.test.js
@@ -1,29 +1,59 @@
 const geonode = require('../index');
 
-describe('find', () => {
+describe('test geo node api', () => {
 
-    it('find a country by code', () => {
-        const countryData = geonode.find.country("IN");
-        expect(countryData.name).toBe("India");
-        expect(countryData.cca2).toBe("IN");
-    });
+    describe('test country api', () => {
 
-    it('find a country by name', () => {
-        const countryData = geonode.find.country("india");
-        expect(countryData.name).toBe("India");
-        expect(countryData.cca2).toBe("IN");
-    });
+        it('should find a country by code', () => {
+            const countryData = geonode.find.country("IN");
+            expect(countryData.name).toBe("India");
+            expect(countryData.cca2).toBe("IN");
+        });
+    
+        it('should find a country by name', () => {
+            const countryData = geonode.find.country("india");
+            expect(countryData.name).toBe("India");
+            expect(countryData.cca2).toBe("IN");
+        });
+        
+        it('should find a country by best match name', () => {
+            const countryData = geonode.find.country("indian");
+            expect(countryData.name).toBe("India");
+            expect(countryData.cca2).toBe("IN");
+        });
+        
+        it('should return undefined in case of bad country name', () => {
+            const countryData = geonode.find.country("blah blah");
+            expect(countryData).toBeUndefined();            
+        });
 
-    it('find a region by name', () => {
-        const regionData = geonode.find.region("karnataka");
-        expect(regionData.name).toBe("Karnataka");
-        expect(regionData.cca2).toBe("KA");
-    });
+    });    
 
-    it('find a region by code', () => {
-        const regionData = geonode.find.region("ka");
-        expect(regionData.name).toBe("Karnataka");
-        expect(regionData.cca2).toBe("KA");
+    describe('test region api', () => {
+
+        it('should find a region by code', () => {
+            const regionData = geonode.find.region("ka");
+            expect(regionData.name).toBe("Karnataka");
+            expect(regionData.cca2).toBe("KA");
+        });
+    
+        it('should find a region by name', () => {
+            const regionData = geonode.find.region("karnataka");
+            expect(regionData.name).toBe("Karnataka");
+            expect(regionData.cca2).toBe("KA");
+        });
+    
+        it('should find a region by best match name', () => {
+            const regionData = geonode.find.region("Andra Pradesh");
+            expect(regionData.name).toBe("Andhra Pradesh");
+            expect(regionData.cca2).toBe("AP");
+        });
+    
+        it('should return undefined in case of bad region name', () => {
+            const regionData = geonode.find.region("blah blah");
+            expect(regionData).toBeUndefined();
+        });
+
     });
 
 });
